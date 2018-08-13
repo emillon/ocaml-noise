16577d054c9b1775653b3647e0ffd15b48a91280
Etienne Millon <me@emillon.org>
2018-08-13 08:59:03 +0000

    Do not go through hex

--- lib/hash_sha256.ml
+++ lib/hash_sha256.ml
@@ -1,17 +1,13 @@
-let digest_to_string d =
-  let from_hex_string s =
-    Hex.to_cstruct (`Hex s)
-  in
-  Digestif.SHA256.to_hex d
-  |> from_hex_string
+let to_cstruct (d:Digestif.SHA256.t) =
+  Cstruct.of_string (d :> string)
 
 let hash data =
   Cstruct.to_string data
   |> Digestif.SHA256.digest_string
-  |> digest_to_string
+  |> to_cstruct
 
 let hmac ~key data =
   let string_key = Cstruct.to_string key in
   Cstruct.to_string data
   |> Digestif.SHA256.hmac_string ~key:string_key
-  |> digest_to_string
+  |> to_cstruct
--- lib/hash_sha512.ml
+++ lib/hash_sha512.ml
@@ -1,17 +1,13 @@
-let digest_to_string d =
-  let from_hex_string s =
-    Hex.to_cstruct (`Hex s)
-  in
-  Digestif.SHA512.to_hex d
-  |> from_hex_string
+let to_cstruct (d:Digestif.SHA512.t) =
+  Cstruct.of_string (d :> string)
 
 let hash data =
   Cstruct.to_string data
   |> Digestif.SHA512.digest_string
-  |> digest_to_string
+  |> to_cstruct
 
 let hmac ~key data =
   let string_key = Cstruct.to_string key in
   Cstruct.to_string data
   |> Digestif.SHA512.hmac_string ~key:string_key
-  |> digest_to_string
+  |> to_cstruct
